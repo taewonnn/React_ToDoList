2bff247a3cbdc01fa4558fd8665b6f39ed5af757
TaeWon Park <thisis_tw94@naver.com>
2023-05-09 20:51:38 +0900

    #6.15 immutability

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -12,7 +12,7 @@ export default function ToDo({ text, category, id }:IToDo) {
     setToDos((oldToDos) => {
       const targetIndex = oldToDos.findIndex(toDO => toDO.id === id);
       const oldToDos = oldToDos[targetIndex];
-      const newTpDos ={text: text, id: id, category: "name"}
+      const newToDos ={text: text, id: id, category: "name"};
 
       return oldToDos;
     })
@@ -66,3 +66,18 @@ export default function ToDo({ text, category, id }:IToDo) {
 //         "category": "TO_DO"
 //     }
 // ]
+
+
+// 배열의 원소 교체 방법
+// const food = ['pizza', 'mango', 'kimchi', 'kimbab']
+
+// mango -> gam으로 바꾸고 싶을 떄
+
+// 1. mango의 위치를 찾기
+
+// 2. 배열을 두 가지로 나눠!  망고 이전의 배열, 망고 이후의 배열
+// const front = ['pizza']    const back = ['kimchi', 'kimbab']
+
+// 이 때 mango를 삭제하고 두 배열로 나누는 방법은 ?? -> slice() 사용;
+
+// 3. finalPart = [...front, "gam", ...back]
